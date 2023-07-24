589316d26496a7130721461171d7d842be3fbc06
resenzhu <resen.zhu@gmail.com>
2023-07-24 15:03:11 +0700

    add functional chatbot

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,12 +1,24 @@
 import {Server, Socket} from 'socket.io';
 import logger from '@utils/logger';
 
+const {dockStart} = require('@nlpjs/basic');
+
 const mainRouter = (server: Server) => {
   const main = server.of('/main');
+  let chatbot: any = null;
 
   main.on('connection', (socket: Socket): void => {
     const mainLogger = logger.child({scope: 'main', socketid: socket.id});
     mainLogger.info('socket connected');
+
+    socket.on('ask-chatbot', async (): Promise<void> => {
+      if (!chatbot) {
+        chatbot = await dockStart().then((dock: any): void => dock.get('nlp'));
+      }
+      chatbot
+        .process('en', 'what is your birthday')
+        .then((reply: any): void => console.log(reply.answer));
+    });
   });
 };
 
