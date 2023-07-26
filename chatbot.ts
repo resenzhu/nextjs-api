bc2209bdd7175b731f3a3d5aa43a1f16dec4dc88
resenzhu <resen.zhu@gmail.com>
2023-07-26 11:33:20 +0700

    add dockstart config

--- chatbot.ts
+++ chatbot.ts
@@ -2,7 +2,14 @@
 
 const {dockStart} = require('@nlpjs/basic');
 
-dockStart().then((dock) => {
+dockStart({
+  use: ['Basic'],
+  settings: {
+    nlp: {
+      corpora: ['chatbot.json']
+    }
+  }
+}).then((dock) => {
   const nlp = dock.get('nlp');
   nlp.train().then(() => {
     nlp.save('chatbot.nlp');
