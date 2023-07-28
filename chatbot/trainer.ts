a8686ebc9b6e1269e5a7663769beb98afa073bc1
Resen <resen.zhu@gmail.com>
2023-07-28 20:26:14 +0700

    add languages and force ner

--- chatbot/trainer.ts
+++ chatbot/trainer.ts
@@ -4,6 +4,8 @@ dockStart({
   use: ['Basic', 'LangEn', 'LangId'],
   settings: {
     nlp: {
+      forceNER: true,
+      languages: ['en', 'id'],
       corpora: ['chatbot/corpus-en.json', 'chatbot/corpus-id.json']
     }
   }
