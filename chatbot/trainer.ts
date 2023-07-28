e2c1dd5fb144f6329d952287fb18f7fd738bef48
Resen <resen.zhu@gmail.com>
2023-07-28 19:58:50 +0700

    add chatbot trainer

---
+++ chatbot/trainer.ts
@@ -0,0 +1,15 @@
+const {dockStart} = require('@nlpjs/basic'); // eslint-disable-line
+
+dockStart({
+  use: ['Basic', 'LangEn', 'LangId'],
+  settings: {
+    nlp: {
+      corpora: ['chatbot/corpus-en.json', 'chatbot/corpus-id.json']
+    }
+  }
+}).then((dock) => {
+  const nlp = dock.get('nlp');
+  nlp.train().then(() => {
+    nlp.save('chatbot/model.nlp');
+  });
+});
