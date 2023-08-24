7d760fa887f59f4687a0750107e4bd597ab2b19d
Resen <resen.zhu@gmail.com>
2023-08-24 13:24:23 +0700

    add response

---
+++ src/utils/response.tsx
@@ -0,0 +1,41 @@
+export type ErrorResponse = {
+  success: false;
+  error: {
+    code: number;
+    message: string;
+  };
+  data: {};
+};
+
+export type SuccessResponse = {
+  success: true;
+  error: {};
+  data: object;
+};
+
+export const createErrorResponse = ({
+  code,
+  message
+}: {
+  code: number | undefined;
+  message: string | undefined;
+}): ErrorResponse => ({
+  success: false,
+  error: {
+    code: code ?? 400,
+    message:
+      message ??
+      'the server cannot process the request due to invalid syntax or malformed structure'
+  },
+  data: {}
+});
+
+export const createSuccessResponse = ({
+  data = {}
+}: {
+  data: object;
+}): SuccessResponse => ({
+  success: true,
+  error: {},
+  data: data
+});
