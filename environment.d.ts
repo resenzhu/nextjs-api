45d64fb00d6989b89d4b88929e5fcb3beb613d84
Resen <resen.zhu@gmail.com>
2023-08-24 21:49:28 +0700

    add mailjet env variables

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,10 @@ declare namespace NodeJS {
   interface ProcessEnv {
     APP_CLIENT: string;
     APP_PORT: number;
+    MAILJET_KEY_API: string;
+    MAILJET_KEY_SECRET: string;
+    MAILJET_USER_NAME: string;
+    MAILJET_USER_EMAIL: string;
     NODE_ENV: 'development' | 'production';
   }
 }
