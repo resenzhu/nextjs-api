71f889410963b59551e9b361bebf1f1a01227b78
Resen <resen.zhu@gmail.com>
2023-08-24 23:23:15 +0700

    add email

---
+++ src/utils/email.ts
@@ -0,0 +1,5 @@
+import Mailjet from 'node-mailjet';
+
+export const sendEmail = ({name, email, message}: {name: string, email: string, password: string}): Promise<void> => (
+  new Promise((resolve, reject): void => {})
+);
