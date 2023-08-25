3282b110ed8f254c0da25ec1f2cf2a664fd9fb9f
resenzhu <resen.zhu@gmail.com>
2023-08-25 11:27:30 +0700

    add mailjet function

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,5 +1,44 @@
 import Mailjet from 'node-mailjet';
 
-export const sendEmail = ({name, email, message}: {name: string, email: string, password: string}): Promise<void> => (
-  new Promise((resolve, reject): void => {})
-);
+export const sendEmail = ({
+  name,
+  email,
+  message
+}: {
+  name: string;
+  email: string;
+  message: string;
+}): Promise<string> =>
+  new Promise((resolve, reject): void => {
+    const mailjet = Mailjet.apiConnect(
+      process.env.MAILJET_KEY_API,
+      process.env.MAILJET_KEY_SECRET
+    );
+    mailjet
+      .post('send', {version: 'v3.1'})
+      .request({
+        Messages: [
+          {
+            From: {
+              Name: process.env.MAILJET_USER_NAME,
+              Email: process.env.MAILJET_USER_EMAIL
+            },
+            To: [
+              {
+                Name: process.env.MAILJET_USER_NAME,
+                Email: process.env.MAILJET_USER_EMAIL
+              }
+            ],
+            Subject: `Message from ${name} <${email}>`,
+            TextPart: message.replaceAll('\n', '\\n'),
+            HTMLPart: message.replaceAll('\n', '<br />')
+          }
+        ]
+      })
+      .then((): void => {
+        resolve('OK');
+      })
+      .catch((error): void => {
+        reject(error.ErrorMessage);
+      });
+  });
