e2f3c94b9373904f9036b2da93b41483a2a512b1
resenzhu <resen.zhu@gmail.com>
2023-08-25 11:28:21 +0700

    add sendEmail and update variables and strings

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -3,6 +3,7 @@ import {createErrorResponse, createSuccessResponse} from '@utils/response';
 import joi from 'joi';
 import logger from '@utils/logger';
 import {sanitize} from 'isomorphic-dompurify';
+import {sendEmail} from '@utils/email';
 
 type AskChatbotReq = {
   input: string;
@@ -57,25 +58,26 @@ const mainRouter = async (server: Server): Promise<void> => {
         mainLogger.info({request: request}, 'ask chatbot');
         const requestSchema = joi.object({
           input: joi.string().min(1).max(160).required().messages({
-            'string.base': "4220101|'input' must be a string",
-            'string.empty': "4220102|'input' must not be empty",
+            'string.base': "4220101|'input' must be a string.",
+            'string.empty': "4220102|'input' must not be empty.",
             'string.min':
-              "4220103|'input' must be between 1 and 160 characters",
+              "4220103|'input' must be between 1 and 160 characters.",
             'string.max':
-              "4220104|'input' must be between 1 and 160 characters",
-            'any.required': "40001|'input' is required"
+              "4220104|'input' must be between 1 and 160 characters.",
+            'any.required': "40001|'input' is required."
           })
         });
-        const {value, error} = requestSchema.validate(request);
-        if (error) {
+        const {value: validatedValue, error: validationError} =
+          requestSchema.validate(request);
+        if (validationError) {
           const response: AskChatbotRes = createErrorResponse({
-            code: error.message.split('|')[0],
-            message: error.message.split('|')[1]
+            code: validationError.message.split('|')[0],
+            message: validationError.message.split('|')[1]
           });
           mainLogger.warn({response: response}, 'ask chatbot failed');
           callback(response);
         }
-        const data = value as AskChatbotReq;
+        const data = validatedValue as AskChatbotReq;
         const reply = await chatbot.process(sanitize(data.input).trim());
         const response: AskChatbotRes = createSuccessResponse({
           data: {
@@ -101,53 +103,76 @@ const mainRouter = async (server: Server): Promise<void> => {
             .pattern(/^[a-zA-Z\s]*$/u)
             .required()
             .messages({
-              'string.base': "4220101|'name' must be a string",
-              'string.empty': "4220102|'name' must not be empty",
+              'string.base': "4220101|'name' must be a string.",
+              'string.empty': "4220102|'name' must not be empty.",
               'string.min':
-                "4220103|'name' must be between 2 and 120 characters",
+                "4220103|'name' must be between 2 and 120 characters.",
               'string.max':
-                "4220104|'name' must be between 2 and 120 characters",
+                "4220104|'name' must be between 2 and 120 characters.",
               'string.pattern.base':
-                "4220105|'name' must only contain letters and spaces",
-              'any.required': "40001|'name' is required"
+                "4220105|'name' must only contain letters and spaces.",
+              'any.required': "40001|'name' is required."
             }),
           email: joi.string().min(3).max(320).email().required().messages({
-            'string.base': "4220201|'email' must be a string",
-            'string.empty': "4220202|'email' must not be empty",
+            'string.base': "4220201|'email' must be a string.",
+            'string.empty': "4220202|'email' must not be empty.",
             'string.min':
-              "4220203|'email' must be between 3 and 320 characters",
+              "4220203|'email' must be between 3 and 320 characters.",
             'string.max':
-              "4220204|'email' must be between 3 and 320 characters",
-            'string.email': "4220205|'email' must be in a valid format",
-            'any.required': "40002|'email' is required"
+              "4220204|'email' must be between 3 and 320 characters.",
+            'string.email': "4220205|'email' must be in a valid format.",
+            'any.required': "40002|'email' is required."
           }),
           message: joi.string().min(15).max(2000).required().messages({
-            'string.base': "4220301|'message' must be a string",
-            'string.empty': "4220302|'message' must not be empty",
+            'string.base': "4220301|'message' must be a string.",
+            'string.empty': "4220302|'message' must not be empty.",
             'string.min':
-              "4220303|'message' must be between 15 and 2000 characters",
+              "4220303|'message' must be between 15 and 2000 characters.",
             'string.max':
-              "4220304|'message' must be between 15 and 2000 characters",
-            'any.required': "40003|'message' is required"
+              "4220304|'message' must be between 15 and 2000 characters.",
+            'any.required': "40003|'message' is required."
           }),
           honeypot: joi.string().allow('').length(0).required().messages({
-            'string.base': "4220401|'honeypot' must be a string",
-            'string.length': "4220402|'honeypot' must be empty",
-            'any.required': "40004|'honeypot' is required"
+            'string.base': "4220401|'honeypot' must be a string.",
+            'string.length': "4220402|'honeypot' must be empty.",
+            'any.required': "40004|'honeypot' is required."
           })
         });
-        const {error} = requestSchema.validate(request);
-        if (error) {
+        const {value: validatedValue, error: validationError} =
+          requestSchema.validate(request);
+        if (validationError) {
           const response: SubmitContactFormRes = createErrorResponse({
-            code: error.message.split('|')[0],
-            message: error.message.split('|')[1]
+            code: validationError.message.split('|')[0],
+            message: validationError.message.split('|')[1]
           });
           mainLogger.warn({response: response}, 'submit contact form failed');
           callback(response);
         }
-        const response: SubmitContactFormRes = createSuccessResponse({});
-        mainLogger.info({response: response}, 'submit contact form success');
-        callback(response);
+        const data = validatedValue as SubmitContactFormReq;
+        sendEmail({
+          name: sanitize(data.name).trim(),
+          email: sanitize(data.email).trim(),
+          message: sanitize(data.message).trim()
+        })
+          .then((): void => {
+            const response: SubmitContactFormRes = createSuccessResponse({});
+            mainLogger.info(
+              {response: response},
+              'submit contact form success'
+            );
+            callback(response);
+          })
+          .catch((error): void => {
+            const response: SubmitContactFormRes = createErrorResponse({
+              code: '500',
+              message: 'an error occured while attempting to send the email.'
+            });
+            mainLogger.warn(
+              {response: response, error: error},
+              'submit contact form failed'
+            );
+            callback(response);
+          });
       }
     );
     socket.on('disconnect', (): void => {
