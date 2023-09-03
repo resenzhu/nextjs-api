24f51b353dbd8c6870e9be2f51a89a81f42d873c
Resen <resen.zhu@gmail.com>
2023-09-03 23:46:45 +0700

    add recaptcha

---
+++ src/utils/recaptcha.ts
@@ -0,0 +1,23 @@
+import axios, {type AxiosResponse} from 'axios';
+
+export const verifyCaptcha = ({
+  token
+}: {
+  token: string;
+}): Promise<number | string> =>
+  new Promise<number | string>((resolve, reject): void => {
+    axios
+      .post(
+        'https://www.google.com/recaptcha/api/siteverify',
+        new URLSearchParams({
+          secret: process.env.GOOGLE_RECAPTCHA_KEY,
+          response: token
+        })
+      )
+      .then((response: AxiosResponse): void => {
+        if (!response.data.success) {
+          reject(response.data['error-codes'][0] as string);
+        }
+        resolve(response.data.score as number);
+      });
+  });
