0b285af1ab0dfc22a553577ae862f350ca4b5abb
Resen <resen.zhu@gmail.com>
2023-09-03 23:45:53 +0700

    add google recaptcha key

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     APP_PORT: number;
     APP_CLIENT: string;
+    GOOGLE_RECAPTCHA_KEY: string;
     MAILJET_KEY_API: string;
     MAILJET_KEY_SECRET: string;
     MAILJET_USER_NAME: string;
