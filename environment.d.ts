083ec9f93199fd78c104860d5e821e5e6c2705d2
resenzhu <resen.zhu@gmail.com>
2023-09-04 14:43:51 +0700

    add recaptcha v2 and v3 variables.

--- environment.d.ts
+++ environment.d.ts
@@ -2,7 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     APP_PORT: number;
     APP_CLIENT: string;
-    GOOGLE_RECAPTCHA_KEY: string;
+    GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX: string;
+    GOOGLE_RECAPTCHA_KEY_V3: string;
     MAILJET_KEY_API: string;
     MAILJET_KEY_SECRET: string;
     MAILJET_USER_NAME: string;
