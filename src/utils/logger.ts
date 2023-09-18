67f897b89d736c48165a309a0a927a8dc449a215
resenzhu <resen.zhu@gmail.com>
2023-09-18 16:45:59 +0700

    export logger as default

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,11 @@
 import {DateTime} from 'luxon';
 import pino from 'pino';
 
-export const logger = pino({
+const logger = pino({
   transport: {
     target: 'pino-pretty'
   },
   timestamp: (): string => `,"timestamp":"${DateTime.utc().toISO()}"`
 });
+
+export default logger;
