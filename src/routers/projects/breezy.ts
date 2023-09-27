0e9d18bf97afc53c346a8a3646939a3e5cc2eed6
resenzhu <resen.zhu@gmail.com>
2023-09-27 16:58:28 +0700

    add login event

--- src/routers/projects/breezy.ts
+++ src/routers/projects/breezy.ts
@@ -23,6 +23,13 @@ type SignUpReq = {
   token: string;
 };
 
+type LoginReq = {
+  username: string;
+  password: string;
+  honeypot: string;
+  token: string;
+};
+
 type User = {
   id: string;
   username: string;
@@ -237,6 +244,111 @@ const breezyRouter = (server: Server): void => {
             breezyLogger.warn(
               {response: response, error: error.message},
               'signup failed'
+            );
+            return callback(response);
+          });
+        return undefined;
+      }
+    );
+    socket.on(
+      'login',
+      (
+        request: LoginReq,
+        callback: (response: ClientResponse) => void
+      ): void => {
+        breezyLogger.info({request: request}, 'login');
+        const requestSchema = joi.object({
+          username: joi
+            .string()
+            .min(2)
+            .max(15)
+            .pattern(/^[a-zA-Z0-9_-]+$/u)
+            .required()
+            .messages({
+              'string.base': "4220101|'username' must be a string.",
+              'string.empty': "4220102|'username' must not be empty.",
+              'string.min':
+                "4220103|'username' must be between 2 and 15 characters.",
+              'string.max':
+                "4220104|'username' must be between 2 and 15 characters.",
+              'string.pattern.base':
+                "4220105|'username' must only contain letters, numbers, hyphen, and underscore.",
+              'any.required': "40001|'username' is required."
+            }),
+          password: joi.string().min(8).max(64).required().messages({
+            'string.base': "4220201|'password' must be a string.",
+            'string.empty': "4220202|'password' must not be empty.",
+            'string.min':
+              "4220203|'password' must be between 8 and 64 characters.",
+            'string.max':
+              "4220204|'password' must be between 8 and 64 characters.",
+            'any.required': "40002|'password' is required."
+          }),
+          honeypot: joi.string().allow('').length(0).required().messages({
+            'string.base': "4220301|'honeypot' must be a string.",
+            'string.length': "4220302|'honeypot' must be empty.",
+            'any.required': "40003|'honeypot' is required."
+          }),
+          token: joi.string().required().messages({
+            'string.base': "4220401|'token' must be a string.",
+            'string.empty': "4220402|'token' must not be empty.",
+            'any.required': "40004|'token' is required."
+          })
+        });
+        const {value: validatedValue, error: validationError} =
+          requestSchema.validate(request);
+        if (validationError) {
+          const response: ClientResponse = createErrorResponse({
+            code: validationError.message.split('|')[0],
+            message: validationError.message.split('|')[1]
+          });
+          breezyLogger.warn({response: response}, 'login failed');
+          return callback(response);
+        }
+        let data = validatedValue as LoginReq;
+        data = {
+          ...data,
+          username: sanitize(data.username).trim().toLowerCase(),
+          token: sanitize(data.token).trim()
+        };
+        verifyReCaptcha({
+          version: 2,
+          token: data.token
+        })
+          .then((success): void => {
+            if (!success) {
+              const response: ClientResponse = createErrorResponse({
+                code: '40303',
+                message: 'access denied for bot form submission.'
+              });
+              breezyLogger.warn({response: response}, 'login failed');
+              return callback(response);
+            }
+            breezyStorage
+              .then((): void => {
+                getItem('users').then((users: User[]): void => {});
+              })
+              .catch((error: Error): void => {
+                const response: ClientResponse = createErrorResponse({
+                  code: '500',
+                  message: 'an error occured while accessing the storage.'
+                });
+                breezyLogger.warn(
+                  {response: response, error: error.message},
+                  'login failed'
+                );
+                return callback(response);
+              });
+            return undefined;
+          })
+          .catch((error: Error): void => {
+            const response: ClientResponse = createErrorResponse({
+              code: '500',
+              message: 'an error occured while attempting to verify captcha.'
+            });
+            breezyLogger.warn(
+              {response: response, error: error.message},
+              'login failed'
             );
             return callback(response);
           });
