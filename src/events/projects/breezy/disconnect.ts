7a56f3bf5c53e0c28bb6e589c205310ea8476eb6
Resen <resen.zhu@gmail.com>
2023-10-12 10:17:00 +0700

    change sessions to users

--- src/events/projects/breezy/disconnect.ts
+++ src/events/projects/breezy/disconnect.ts
@@ -1,30 +1,32 @@
 import {getItem, setItem} from 'node-persist';
 import {DateTime} from 'luxon';
 import type {Logger} from 'pino';
-import type {Session} from '@events/projects/breezy/signup';
 import type {Socket} from 'socket.io';
+import type {User} from '@events/projects/breezy/signup';
 import {storage} from '@utils/storage';
 
 const disconnectEvent = (socket: Socket, logger: Logger): void => {
   socket.on('disconnect', (): void => {
     logger.info('socket disconnected');
     storage.then((): void => {
-      getItem('breezy sessions').then((sessions: Session[]): void => {
-        const newSessions = sessions?.map((session): Session => {
-          if (session.socket === socket.id) {
-            const newSession: Session = {
-              ...session,
-              socket: null,
-              status: 'offline',
-              lastOnline:
-                DateTime.utc().toISO() ?? new Date(Date.now()).toISOString()
+      getItem('breezy users').then((users: User[]): void => {
+        const updatedUsers = users?.map((user): User => {
+          if (user.session.socket === socket.id) {
+            const updatedUser: User = {
+              ...user,
+              session: {
+                ...user.session,
+                socket: null,
+                status: 'offline',
+                lastOnline: DateTime.utc().toISO() ?? new Date().toISOString()
+              }
             };
-            return newSession;
+            return updatedUser;
           }
-          return session;
+          return user;
         });
-        if (newSessions) {
-          setItem('breezy sessions', newSessions);
+        if (updatedUsers) {
+          setItem('breezy users', updatedUsers);
         }
       });
     });
