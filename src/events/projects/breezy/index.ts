0c3923794aa63dd4de2a019df2e2f16f47a1ad92
Resen <resen.zhu@gmail.com>
2023-10-13 23:30:13 +0700

    add connectEvent

--- src/events/projects/breezy/index.ts
+++ src/events/projects/breezy/index.ts
@@ -1,3 +1,4 @@
+export {default as connect} from '@events/projects/breezy/connect';
 export {default as disconnect} from '@events/projects/breezy/disconnect';
 export {default as login} from '@events/projects/breezy/login';
 export {default as signup} from '@events/projects/breezy/signup';
