7137f5d3f509100f76959fad8ad76cbe767ddf72
resenzhu <resen.zhu@gmail.com>
2023-10-17 11:15:33 +0700

    change token logic

--- src/events/projects/breezy/connect.ts
+++ src/events/projects/breezy/connect.ts
@@ -19,64 +19,62 @@ const redact: string[] = ['token'];
 const connectEvent = (socket: Socket, logger: Logger): void => {
   logger.info('socket connected');
   const {token} = socket.handshake.auth;
-  if (token) {
-    verify(
-      token,
-      Buffer.from(process.env.JWT_KEY_PRIVATE_BASE64, 'base64').toString(),
-      // eslint-disable-next-line
-      (jwtError: VerifyErrors | null, decoded: any): void => {
-        logger.info({token: token}, 'user online');
-        if (jwtError) {
-          logger.warn({error: jwtError.message}, 'user online failed');
-          socket.disconnect();
-        }
-        const payload = decoded as Token;
-        storage
-          .then((): void => {
-            getItem('breezy users').then((users: User[]): void => {
-              const updatedUsers = users?.map((user): User => {
-                if (
-                  user.id === payload.id &&
-                  user.session.id === payload.session
-                ) {
-                  const updatedUser: User = {
-                    ...user,
-                    session: {
-                      ...user.session,
-                      socket: socket.id,
-                      status: 'online',
-                      lastOnline:
-                        DateTime.utc().toISO() ?? new Date().toISOString()
-                    }
-                  };
-                  return updatedUser;
-                }
-                return user;
-              });
-              if (updatedUsers) {
-                const ttl = DateTime.max(
-                  ...updatedUsers.map(
-                    (user): DateTime =>
-                      DateTime.fromISO(user.session.lastOnline, {zone: 'utc'})
-                  )
-                )
-                  .plus({months: 1})
-                  .diff(DateTime.utc(), ['milliseconds']).milliseconds;
-                setItem('breezy users', updatedUsers, {ttl: ttl}).then(
-                  (): void => {
-                    logger.info('user online success');
-                  }
-                );
-              }
-            });
-          })
-          .catch((storageError: Error): void => {
-            logger.warn({error: storageError.message}, 'user online failed');
-            socket.disconnect();
-          });
+  verify(
+    token ?? '',
+    Buffer.from(process.env.JWT_KEY_PRIVATE_BASE64, 'base64').toString(),
+    // eslint-disable-next-line
+    (jwtError: VerifyErrors | null, decoded: any): void => {
+      logger.info({token: token}, 'user online');
+      if (jwtError) {
+        logger.warn({error: jwtError.message}, 'user online failed');
+        socket.disconnect();
       }
-    );
-  }
+      const payload = decoded as Token;
+      storage
+        .then((): void => {
+          getItem('breezy users').then((users: User[]): void => {
+            const updatedUsers = users?.map((user): User => {
+              if (
+                user.id === payload.id &&
+                user.session.id === payload.session
+              ) {
+                const updatedUser: User = {
+                  ...user,
+                  session: {
+                    ...user.session,
+                    socket: socket.id,
+                    status: 'online',
+                    lastOnline:
+                      DateTime.utc().toISO() ?? new Date().toISOString()
+                  }
+                };
+                return updatedUser;
+              }
+              return user;
+            });
+            if (updatedUsers) {
+              const ttl = DateTime.max(
+                ...updatedUsers.map(
+                  (user): DateTime =>
+                    DateTime.fromISO(user.session.lastOnline, {zone: 'utc'})
+                )
+              )
+                .plus({months: 1})
+                .diff(DateTime.utc(), ['milliseconds']).milliseconds;
+              setItem('breezy users', updatedUsers, {ttl: ttl}).then(
+                (): void => {
+                  logger.info('user online success');
+                }
+              );
+            }
+          });
+        })
+        .catch((storageError: Error): void => {
+          logger.warn({error: storageError.message}, 'user online failed');
+          socket.disconnect();
+        });
+    }
+  );
 };
 
 export {redact};
