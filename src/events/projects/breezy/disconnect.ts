325ff87835d8621c9c1983aaf98186ee1c1d22e4
Resen <resen.zhu@gmail.com>
2023-10-22 15:28:36 +0700

    add user offline notif

--- src/events/projects/breezy/disconnect.ts
+++ src/events/projects/breezy/disconnect.ts
@@ -5,11 +5,22 @@ import type {Socket} from 'socket.io';
 import type {User} from '@events/projects/breezy/signup';
 import {storage} from '@utils/storage';
 
+type UserOfflineNotif = {
+  user: {
+    id: string;
+    session: {
+      status: 'online' | 'away' | 'offline';
+      lastOnline: string;
+    };
+  };
+};
+
 const disconnectEvent = (socket: Socket, logger: Logger): void => {
   socket.on('disconnect', (): void => {
     logger.info('socket disconnected');
     storage.then((): void => {
       getItem('breezy users').then((users: User[] | undefined): void => {
+        let offlineUser: User | null = null;
         const updatedUsers = users?.map((user): User => {
           if (user.session.socket === socket.id) {
             const updatedUser: User = {
@@ -21,6 +32,7 @@ const disconnectEvent = (socket: Socket, logger: Logger): void => {
                 lastOnline: DateTime.utc().toISO() ?? new Date().toISOString()
               }
             };
+            offlineUser = updatedUser;
             return updatedUser;
           }
           return user;
@@ -34,7 +46,20 @@ const disconnectEvent = (socket: Socket, logger: Logger): void => {
           )
             .plus({weeks: 1})
             .diff(DateTime.utc(), ['milliseconds']).milliseconds;
-          setItem('breezy users', updatedUsers, {ttl: ttl});
+          setItem('breezy users', updatedUsers, {ttl: ttl}).then((): void => {
+            if (offlineUser) {
+              const userOfflineNotif: UserOfflineNotif = {
+                user: {
+                  id: offlineUser.id,
+                  session: {
+                    status: offlineUser.session.status,
+                    lastOnline: offlineUser.session.lastOnline
+                  }
+                }
+              };
+              socket.broadcast.emit('user offline', userOfflineNotif);
+            }
+          });
         }
       });
     });
