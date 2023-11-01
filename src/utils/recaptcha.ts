c39b3aaccad37b4c00aceb004e9da15010e89e77
resenzhu <resen.zhu@gmail.com>
2023-11-01 16:55:58 +0700

    add test logic

--- src/utils/recaptcha.ts
+++ src/utils/recaptcha.ts
@@ -8,21 +8,28 @@ export const verifyRecaptcha = ({
   recaptcha: string;
 }): Promise<boolean | number | string> =>
   new Promise<boolean | number | string>((resolve, reject): void => {
-    axios
-      .post(
-        'https://www.google.com/recaptcha/api/siteverify',
-        new URLSearchParams({
-          secret:
-            version === 2
-              ? process.env.GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX
-              : process.env.GOOGLE_RECAPTCHA_KEY_V3,
-          response: recaptcha
-        })
-      )
-      .then((response): void => {
-        if (!response.data.success) {
-          reject(new Error(response.data['error-codes'][0]));
-        }
-        resolve(version === 2 ? response.data.success : response.data.score);
-      });
+    if (
+      process.env.NODE_ENV !== 'production' &&
+      recaptcha === process.env.APP_CLIENT_RECAPTCHA_DUMMY
+    ) {
+      resolve(version === 2 ? true : 10);
+    } else {
+      axios
+        .post(
+          'https://www.google.com/recaptcha/api/siteverify',
+          new URLSearchParams({
+            secret:
+              version === 2
+                ? process.env.GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX
+                : process.env.GOOGLE_RECAPTCHA_KEY_V3,
+            response: recaptcha
+          })
+        )
+        .then((response): void => {
+          if (!response.data.success) {
+            reject(new Error(response.data['error-codes'][0]));
+          }
+          resolve(version === 2 ? response.data.success : response.data.score);
+        });
+    }
   });
