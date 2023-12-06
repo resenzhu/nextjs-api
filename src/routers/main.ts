5b9de35db1be8796e485459481ecfab4486cb169
Resen <resen.zhu@gmail.com>
2023-12-06 20:48:04 +0700

    change logger redact logic

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,31 +1,31 @@
 import {askChatbot, disconnect, submitContactForm} from '@events/main';
+import {getRedaction, logger} from '@utils/logger';
 import type {Server} from 'socket.io';
-import logger from '@utils/logger';
-import {redact as submitContactFormRedact} from '@events/main/submit-contact-form';
 
 const {dockStart} = require('@nlpjs/basic'); // eslint-disable-line
 
 const mainRouter = async (server: Server): Promise<void> => {
   const main = server.of('/main');
   const chatbot = await dockStart().then((dock: any): any => dock.get('nlp')); // eslint-disable-line
-  main.on('connection', (socket): void => {
-    const mainRedact = [...submitContactFormRedact];
-    const mainLogger = logger.child(
-      {
-        namespace: 'main',
-        socketid: socket.id
-      },
-      {
-        redact: {
-          paths: mainRedact,
-          censor: '[redacted]'
+  getRedaction({module: '@events/main'}).then((redaction) => {
+    main.on('connection', (socket): void => {
+      const mainLogger = logger.child(
+        {
+          namespace: 'main',
+          socketid: socket.id
+        },
+        {
+          redact: {
+            paths: redaction,
+            censor: '[redacted]'
+          }
         }
-      }
-    );
-    mainLogger.info('socket connected');
-    askChatbot(socket, mainLogger, {chatbot: chatbot});
-    submitContactForm(socket, mainLogger);
-    disconnect(socket, mainLogger);
+      );
+      mainLogger.info('socket connected');
+      askChatbot(socket, mainLogger, {chatbot: chatbot});
+      submitContactForm(socket, mainLogger);
+      disconnect(socket, mainLogger);
+    });
   });
 };
 
