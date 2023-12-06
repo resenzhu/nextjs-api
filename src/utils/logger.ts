28dc87feea739cd661dfed258ee6cb893c27c877
Resen <resen.zhu@gmail.com>
2023-12-06 20:39:47 +0700

    add getRedaction function

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,3 +7,32 @@ export const logger = pino({
   },
   timestamp: (): string => `,"timestamp":"${DateTime.utc().toISO()}"`
 });
+
+export const getRedaction = async ({
+  module
+}: {
+  module: string;
+}): Promise<string[]> => {
+  const submodules = await Object.keys(await import(module));
+  const allRedaction = await Promise.all(
+    submodules.map(
+      async (submodule): Promise<string> =>
+        (
+          await import(
+            `@events/main/${submodule.replace(
+              /[A-Z]/g,
+              (word): string => `-${word.toLowerCase()}`
+            )}`
+          )
+        ).redact
+    )
+  );
+  const definedRedaction = allRedaction
+    .filter((redaction): boolean => redaction !== undefined)
+    .toString()
+    .split(',');
+  const distinctRedaction = definedRedaction.filter(
+    (redaction, index): boolean => definedRedaction.indexOf(redaction) === index
+  );
+  return distinctRedaction;
+};
