94dd6ac4d8815c48929d744a8005c8b3e3a4e6b2
Resen <resen.zhu@gmail.com>
2023-12-20 10:36:11 +0700

    run prettier

--- src/events/main/index.ts
+++ src/events/main/index.ts
@@ -1,3 +1,10 @@
-export {default as askChatbot, type AskChatbotReq} from '@events/main/ask-chatbot';
+export {
+  default as askChatbot,
+  type AskChatbotReq
+} from '@events/main/ask-chatbot';
 export {default as disconnect} from '@events/main/disconnect';
-export {default as submitContactForm, type SubmitContactFormReq, type Submission} from '@events/main/submit-contact-form';
+export {
+  default as submitContactForm,
+  type SubmitContactFormReq,
+  type Submission
+} from '@events/main/submit-contact-form';
--- src/events/projects/breezy/index.ts
+++ src/events/projects/breezy/index.ts
@@ -1,9 +1,24 @@
 export {default as disconnect} from '@events/projects/breezy/disconnect';
 export {default as fetchProfile} from '@events/projects/breezy/fetch-profile';
 export {default as fetchUsers} from '@events/projects/breezy/fetch-users';
-export {default as login, type LoginReq, type UserStatusNotif} from '@events/projects/breezy/login';
+export {
+  default as login,
+  type LoginReq,
+  type UserStatusNotif
+} from '@events/projects/breezy/login';
 export {default as logout} from '@events/projects/breezy/logout';
-export {default as signup, type SignUpReq, type User, type NewUserNotif} from '@events/projects/breezy/signup';
-export {default as updateUserStatus, type UpdateUserStatusReq} from '@events/projects/breezy/update-user-status';
+export {
+  default as signup,
+  type SignUpReq,
+  type User,
+  type NewUserNotif
+} from '@events/projects/breezy/signup';
+export {
+  default as updateUserStatus,
+  type UpdateUserStatusReq
+} from '@events/projects/breezy/update-user-status';
 export {default as verifyStatus} from '@events/projects/breezy/verify-status';
-export {default as verifyToken, type JWTPayload} from '@events/projects/breezy/verify-token';
+export {
+  default as verifyToken,
+  type JWTPayload
+} from '@events/projects/breezy/verify-token';
