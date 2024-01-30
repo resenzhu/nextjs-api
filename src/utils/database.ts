eb1d19437dcf5aead456049eef4d9da9776444a2
resenzhu <resen.zhu@gmail.com>
2024-01-30 11:42:55 +0700

    set mysql connection limit to 100

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,5 +3,6 @@ import mysql from 'mysql2';
 export const database = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD
+  password: process.env.MYSQL_PASSWORD,
+  connectionLimit: 100
 });
