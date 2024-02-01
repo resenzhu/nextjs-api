b3b16528d2f38b57985888ac20007632daedf151
Resen <resen.zhu@gmail.com>
2024-02-01 15:02:42 +0700

    revert mysql2 import and change connectionlimit to 200

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,8 +1,8 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 
 export const database = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  connectionLimit: 100
+  connectionLimit: 200
 });
