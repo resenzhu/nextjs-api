432810e0ec89de9634cbc45e7695e8cd18942e4d
Resen <resen.zhu@gmail.com>
2024-02-04 15:53:58 +0700

    change breezyLogger declaration logic

--- src/routers/projects/breezy.ts
+++ src/routers/projects/breezy.ts
@@ -8,16 +8,20 @@ import {
   signup,
   updateUserStatus
 } from '@events/projects/breezy';
+import type {Logger} from 'pino';
 import type {Server} from 'socket.io';
 
 const breezyRouter = (server: Server): void => {
   const breezy = server.of('/project/breezy');
+  let breezyLogger: Logger | null = null;
   getRedaction({module: '@events/projects/breezy'}).then((redaction): void => {
     breezy.on('connection', (socket): void => {
-      const breezyLogger = createRouterLogger({
-        socket: socket,
-        redaction: redaction
-      });
+      if (!breezyLogger) {
+        breezyLogger = createRouterLogger({
+          socket: socket,
+          redaction: redaction
+        });
+      }
       breezyLogger.info('socket connected');
       signup(socket, breezyLogger);
       login(socket, breezyLogger);
