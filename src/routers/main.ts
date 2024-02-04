5e815bdcd30d426e7d718f6c308132132b6c0c58
Resen <resen.zhu@gmail.com>
2024-02-04 15:38:26 +0700

    change mainLogger declaration logic

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -1,5 +1,6 @@
 import {askChatbot, disconnect, submitContactForm} from '@events/main';
 import {createRouterLogger, getRedaction} from '@utils/logger';
+import type {Logger} from 'pino';
 import type {Server} from 'socket.io';
 
 const {dockStart} = require('@nlpjs/basic'); // eslint-disable-line
@@ -7,12 +8,15 @@ const {dockStart} = require('@nlpjs/basic'); // eslint-disable-line
 const mainRouter = async (server: Server): Promise<void> => {
   const main = server.of('/main');
   const chatbot = await dockStart().then((dock: any): any => dock.get('nlp')); // eslint-disable-line
+  let mainLogger: Logger | null = null;
   getRedaction({module: '@events/main'}).then((redaction) => {
     main.on('connection', (socket): void => {
-      const mainLogger = createRouterLogger({
-        socket: socket,
-        redaction: redaction
-      });
+      if (!mainLogger) {
+        mainLogger = createRouterLogger({
+          socket: socket,
+          redaction: redaction
+        });
+      }
       mainLogger.info('socket connected');
       askChatbot(socket, mainLogger, {chatbot: chatbot});
       submitContactForm(socket, mainLogger);
